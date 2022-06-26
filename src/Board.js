1228be42a62869a479a8a20ae3bac0068b8dfda0
dogancan.okur <dogancan.okur@gmail.com>
2022-06-27 01:29:55 +0300

    declaring-winner

---
+++ src/Board.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Square from './Square';
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+
+    // birisi kazanmis ise yada alan dolu ise isaretleme yapma
+    if (calculateWinner(squares) || squares[i]) return;
+
+    const xIsNext = this.state.xIsNext;
+    squares[i] = xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !xIsNext
+    });
+  }
+
+  renderSquare(i) {
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
+  }
+
+  reset() {
+    this.setState({
+      squares: Array(9).fill(null),
+      xIsNext: true
+    });
+  }
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div className='text-center'>
+        <div className="text-center status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+
+        <Reset onClick={() => this.reset()} />
+
+      </div>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+
+}
+
+class Reset extends React.Component {
+  render() {
+    return (
+      <button onClick={this.props.onClick}>Reset</button>
+    );
+  }
+}
+export default Board;
